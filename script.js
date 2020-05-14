93b3be0bd05774cd053c2b668c78716209d458bb
astralsweat <azmainfiaz@gmail.com>
2020-05-14 20:19:09 +0600

    Helicon

--- script.js
+++ script.js
@@ -10,15 +10,21 @@ function calcAgeInDays() {
 
           document.querySelector('.green-btn').addEventListener('click', () => {
                     // IF USER INPUT > 0 , DISPLAY RESULT
-                    console.log(ageInput)
                     if (ageInput > 0) {
                               ageInDays = ageInput * 365
+                              document.querySelector('.flexbox-1-2').style.display = 'block'
                               document.querySelector('.flexbox-1-2').textContent = 'You are ' + ageInDays + ' days old.'
                     } else {
                               // ONLY IF USER INPUT is anything else, DISPLAY OTHER RESULT
                               document.querySelector('.flexbox-1-2').textContent = 'You are invalid.'
+                              document.querySelector('.flexbox-1-2').style.color = 'red'
                     }
           })
+}
+
+function resetAgeCalc() {
+          document.querySelector('.age-input').classList.remove('active')
+          document.querySelector('.flexbox-1-2').style.display = 'none'
 }
 
 
